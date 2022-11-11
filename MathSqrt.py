b25a31a833ec1c67de1f9d1b67b5e2a05269295b
lenbo <allenbora@gmail.com>
2022-11-11 09:39:03 +0800

    Tenth

---
+++ MathSqrt.py
@@ -0,0 +1,13 @@
+#Python program to demonstrate sqrt() method.............
+#import the math module....
+
+import math
+
+# print the square root of 64
+print(math.sqrt(64))
+
+# print the square root of 121
+print(math.sqrt(121))
+
+# print the square root of 900
+print(math.sqrt(900))
